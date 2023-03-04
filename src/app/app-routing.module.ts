ff82b458f59f3c2e7d9018929383236528b63ac1
filipporeucci <reu.filippo@gmail.com>
2023-03-04 10:46:47 +0100

    Login button cambia pagina correttamente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
 @NgModule({
