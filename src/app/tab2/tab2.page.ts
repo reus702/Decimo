ee0b2cd3bf35c829b03ef15173bb94b0aaa3fc37
filipporeucci <reu.filippo@gmail.com>
2023-03-06 16:03:48 +0100

    picker numero giocatori

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  private number_player:number;
 
-  constructor() {}
+  constructor(private pickerCtrl: PickerController) {
+    this.number_player = 0;
+  }
+
+  async openPicker() {
+    const picker = await this.pickerCtrl.create({
+      columns: [
+        {
+          name: 'number_player',
+          options: [
+            {
+              text: 'JavaScript',
+              value: 'javascript',
+            }
+          ],
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirm',
+          handler: (value) => {
+            window.alert(`You selected: ${value.number_player.value}`);
+          },
+        },
+      ],
+    });
+
+    await picker.present();
+  }
 
 }
