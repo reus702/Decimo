e1271d71a344db8e5c1f3c6549dd653f4765eace
Diezz01 <diegopennesi01@gmail.com>
2023-04-26 16:52:35 +0200

    Modifiche form registrazione

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl,  Validators, AbstractControl } from '@angular/forms';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -7,15 +8,29 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-  nome:string;
+ nomeUtente:string;
   email:string;
   password:string;
   confirmpwd:string;
   provincia:string;
   signedUpUser: any[] = [];
 
+ //validatori input del form registrazione
+ signupForm = new FormGroup({
+  nome:new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]+$')]),
+  email:new FormControl('',[Validators.email, Validators.required]),
+  password:new FormControl('',[Validators.required, Validators.minLength(8)]),
+  confirmpwd: new FormControl('',[Validators.required]),
+  provincia:new FormControl('',[Validators.required, Validators.minLength(2), Validators.maxLength(2), Validators.pattern('[A-Z]+$')])
+}, [controlloPwd("password","confirmpwd")]);
+//getter vallidatori form registraizone
+  get nomeFormControl(){return this.signupForm.get('nome')}
+  get emailFormControl(){return this.signupForm.get('email')}
+  get passwordFormControl(){return this.signupForm.get('password')}
+  get provinciaFormControl(){return this.signupForm.get('provincia')}
+
   constructor(private userService: UserService) { 
-    this.nome = "";
+    this.nomeUtente = "";
     this.email = "";
     this.password = "";
     this.confirmpwd = "";
@@ -26,8 +41,15 @@ export class SignupPage implements OnInit {
   }
 
   signUpUser() {
-    let userInfo: string[] = [this.nome,this.email,this.password,this.provincia];
+    let userInfo: string[] = [this.nomeUtente,this.email,this.password,this.provincia];
     this.userService.signUpUser(userInfo);
   }
 
+}
+//funzione per il conrtollo match delle password in fase di registrazione
+export function controlloPwd(pwd: string, controlPwd: string){
+  return function(form: AbstractControl){
+    if(form.get(pwd)?.value === form.get(controlPwd)?.value) return null
+    return { passwordMismatchError: true}
+  }
 }
